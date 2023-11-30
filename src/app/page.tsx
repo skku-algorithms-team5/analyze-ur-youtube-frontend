faa161dbc7a82b28c54ddbc25d37b27b6db4c450
wooneojun <119714067+wooneojun@users.noreply.github.com>
2023-11-30 18:45:14 +0900

    loadingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
 import Image from 'next/image';
 import { LandingPage } from '@/components/landing-page';
+import { LoaderPage } from '@/components/loader-page';
+
 export default function Home() {
   return (
     <main>
-      <LandingPage />
+      <LoaderPage />
     </main>
   );
 }
