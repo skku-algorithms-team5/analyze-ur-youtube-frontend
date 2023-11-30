c8840dfa64bb05a797c95eed2f5da86e81847857
wooneojun <119714067+wooneojun@users.noreply.github.com>
2023-11-30 18:45:14 +0900

    loadingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 'use client';
+<<<<<<< HEAD
 import { RecoilRoot } from 'recoil';
 
 import Page from './home';
+=======
+import Image from 'next/image';
+import { LandingPage } from '@/components/landing-page';
+import { LoaderPage } from '@/components/loader-page';
+>>>>>>> faa161d (loadingpage)
 
 export default function Home() {
   return (
     <main>
+<<<<<<< HEAD
       <RecoilRoot>
         <Page></Page>
       </RecoilRoot>
+=======
+      <LoaderPage />
+>>>>>>> faa161d (loadingpage)
     </main>
   );
 }
