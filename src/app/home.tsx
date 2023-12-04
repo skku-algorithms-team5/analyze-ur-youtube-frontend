6c37ff025fbdb6698e68f2eaa9f24188cb66fd7f
J2yun <alex68566856@gmail.com>
2023-12-05 02:27:34 +0900

    feat: SSE 연결

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -9,52 +9,22 @@ import { LandingPage } from '@/components/landing-page';
 import AuyPage from '@/components/analysis-page/auy-page';
 
 export default function Page() {
-  const setAnalysis = useSetRecoilState(analysisState);
   const [pageStatus, setPageStatsus] = useState<string>('landing');
-  const [isLoadingModalOpen, setIsLoadingModalOpen] = useState<boolean>(false);
+  const [videoUrl, setVidoeUrl] = useState<string>('');
 
   let response;
   let result;
 
-  const submitHandler = (videoUrl: string) => {
+  const urlHandler = (videoUrl: string) => {
     setPageStatsus('analysis');
-    setIsLoadingModalOpen(true);
-    axios
-      .get(`http://127.0.0.1:8000/analyze?url=${videoUrl}`)
-      .then((res) => {
-        const { data } = res;
-        response = data.result;
-
-        response = response.replace('```json', '');
-        response = response.replace('```', '');
-
-        // console.log(response);
-
-        result = JSON.parse(response);
-
-        // console.log(result);
-
-        setAnalysis({
-          posComments: result.pos_comments,
-          negComments: result.neg_comments,
-          analysis: result.analysis,
-          advice: result.advice,
-          repreComment: result.repre_comment,
-        });
-        setPageStatsus('analysis');
-        setIsLoadingModalOpen(false);
-      })
-      .catch((err) => {
-        console.error(err);
-        setPageStatsus('analysis');
-      });
+    setVidoeUrl(videoUrl);
   };
   return (
     <>
       {pageStatus === 'landing' ? (
-        <LandingPage onVideoUrl={submitHandler} />
+        <LandingPage onVideoUrl={urlHandler} />
       ) : (
-        <AuyPage isModalOpen={isLoadingModalOpen} />
+        <AuyPage videoUrl={videoUrl} />
       )}
     </>
   );
--- src/components/analysis-page/bar-graph.tsx
+++ src/components/analysis-page/bar-graph.tsx
@@ -1,17 +1,29 @@
-const BarGraph = () => {
+const BarGraph = ({
+  pos_ratio,
+  neg_ratio,
+}: {
+  pos_ratio: number;
+  neg_ratio: number;
+}) => {
   return (
     <div className="card bg-base-100 w-1/2 min-w-[24.5rem] shadow-xl">
       <div className="card-body">
-        부정적인 댓글
-        <progress
-          className="progress progress-error w-full h-3"
-          value={33}
-          max={100}
-        />
-        긍정적인 댓글
+        <span>
+          긍정적인 댓글{' '}
+          <span className="text-[gray]">({pos_ratio * 100}%)</span>
+        </span>
         <progress
           className="progress progress-success w-full h-3"
-          value={57}
+          value={pos_ratio * 100}
+          max={100}
+        />
+        <span>
+          부정적인 댓글{' '}
+          <span className="text-[gray]">({neg_ratio * 100}%)</span>
+        </span>
+        <progress
+          className="progress progress-error w-full h-3"
+          value={neg_ratio * 100}
           max={100}
         />
       </div>
